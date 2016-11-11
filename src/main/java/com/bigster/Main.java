b97b4933e6b98acdbe050977c52389308c44ed51
yhippa <richard.yhip@gmail.com>
2016-11-11 12:33:38 -0500

    added command line input parsing to main method

--- src/main/java/com/bigster/Main.java
+++ src/main/java/com/bigster/Main.java
@@ -2,6 +2,17 @@ package com.bigster;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello world!");
+        String fileName;
+        String master;
+
+        if (args.length < 1) {
+            System.err.println("At least a filename is required as a command line argument");
+        } else {
+            fileName = args[0];
+        }
+
+        if (args.length == 2) {
+            master = args[1];
+        } else master = null;
     }
 }
