00e59632a9ac6dcd18ae0c096e890c5f0bc250ce
yhippa <richard.yhip@gmail.com>
2016-11-11 16:09:05 -0500

    added apache commons-cli and updated code to handle a few optional arguments

--- src/main/java/com/bigster/Driver.java
+++ src/main/java/com/bigster/Driver.java
@@ -5,12 +5,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 
 public class Driver {
-    public static void main(String[] args) {
 
+    public static void run(String master, String fileName) {
+        System.out.println(master + " " + fileName);
         String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> logData = sc.textFile(logFile).cache();
-        
     }
+
 }
--- src/main/java/com/bigster/Main.java
+++ src/main/java/com/bigster/Main.java
@@ -1,18 +1,38 @@
 package com.bigster;
 
+import org.apache.commons.cli.*;
+
+import java.io.InputStream;
+
 public class Main {
     public static void main(String args[]) {
-        String fileName;
-        String master;
+        CommandLineParser parser = new DefaultParser();
 
-        if (args.length < 1) {
-            System.err.println("At least a filename is required as a command line argument");
-        } else {
-            fileName = args[0];
+        // create the Options
+        Options options = new Options();
+        options.addOption( "d", "debug", false, "Debug mode - print the output to the console");
+        options.addOption( "l", "local", false, "use a local Spark master");
+
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args, true);
+            InputStream resourceAsStream = Main.class.getResourceAsStream(line.getArgs()[0]);
+
+            // validate that block-size has been set
+            if( line.hasOption( "debug" ) ) {
+                System.out.println("user selected debug mode");
+            }
+            if(line.hasOption("local")) {
+                System.out.println("user selected local mode");
+            }
+            if(resourceAsStream == null) {
+                System.err.println("Source '" + line.getArgs()[0] + "' not found.");
+            }
+
+        }
+        catch( ParseException exp ) {
+            System.out.println( "Unexpected exception:" + exp.getMessage() );
         }
 
-        if (args.length == 2) {
-            master = args[1];
-        } else master = null;
     }
 }
